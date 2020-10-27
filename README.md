989b50ecfbe1d3411cf628ad75056dbd8e51f858
zedutra <zedutra93@gmail.com>
2020-10-26 22:20:11 -0300

    files add via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# O que é o ZapMessageRepeater?
+stalkerTool é um programa desenvolvido em Python com a biblioteca Selenium, que realiza o download das primeiras imagens de uma determinada conta do instagram.
+
+# Instalação
+*Requisitos: Python3 e PiP instalados*
+
+1. Faça o download do código com o comando **git clone https://github.com/zedutra/stalkerTool.git**
+2. Instale a biblioteca selenium com o comando **pip install -U selenium**
+3. Execute o arquivo **tool.py**
---
+++ tool.py
@@ -0,0 +1,42 @@
+# Imports
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import time
+import os
+
+
+# Inputs
+user =  str(input('Insira o nome do usuário: '))
+
+
+# Starting webdriver
+options = Options()
+options.page_load_strategy = 'normal'
+options.headless = False
+driver = webdriver.Chrome(options=options)
+driver.get("https://www.instagram.com/" + user)
+
+
+# Closing popup
+driver.implicitly_wait(4)
+driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div/div/button').click()
+
+
+# Downloading photos
+photos = driver.find_elements_by_class_name("_9AhH0")
+if len(photos) > 0:
+    os.mkdir(user)
+    countphotos = 0
+    countscroll = 0
+    while countphotos < len(photos):
+        photos[countphotos].screenshot('./{}/img{}.png'.format(user, countphotos))
+        driver.execute_script("window.scrollTo({}, {});".format(countscroll, countscroll + 100))
+        print('Baixando fotos {}'.format(countphotos))
+        countphotos += 1
+        countscroll += 100
+        time.sleep(1)
+    print('{} fotos baixadas'.format(len(photos)))
+else:
+    print('A conta não possui fotos disponiveis')
+driver.close()
